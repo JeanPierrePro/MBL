b713be59ec0d5e89a6b07ae817218b4705a07a0a
miguelolegna <miguel.olegna1@gmail.com>
2025-05-16 16:40:45 +0100

    navbar2

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 // src/App.tsx
-import React from 'react';
+// import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
